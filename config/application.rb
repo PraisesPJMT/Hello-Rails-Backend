3d686fcfefba7d619f77bf2bfbad2ddd0cc4958e
PraisesPJMT <praisesmusa@outlook.com>
2023-01-11 08:17:10 +0100

    Add CORS features for API requests

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,12 @@ module HelloRailsBackend
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :options]
+      end
+    end
   end
 end
