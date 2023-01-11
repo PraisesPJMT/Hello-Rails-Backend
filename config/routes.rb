5a9644aa2a84c44f2071fd5d66f01778cf13cf6a
PraisesPJMT <praisesmusa@outlook.com>
2023-01-11 08:16:37 +0100

    Add API endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api do
+    namespace :v1 do
+      resources :messages, only: %i[index]
+    end
+  end
 end
